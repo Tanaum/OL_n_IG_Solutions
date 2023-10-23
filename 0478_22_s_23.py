41bf79a9fa5e2a8ac0a8e37313241b12f7ff627f
Tanaum <tanaumbutt@gmail.com>
2023-10-23 21:23:22 +0500

    updated
    the better and prettier (somewhat) version

--- 0478_22_s_23.py
+++ 0478_22_s_23.py
@@ -1,10 +1,7 @@
-#0478/22/M/J/2023
-#arrays/lists that are in the question
-
 Account = [
-    ['Adam Kent', 'k3ntgobrr'],
-    ['Juliette', 'd0ntt0chm3'],
-    ['Pip', 't@lkn3rdy70m3']
+    ['A', 'abc'],
+    ['person', '123'],
+    ['human', 'xyz789']
 ]
 
 AccDetails = [
@@ -13,29 +10,23 @@ AccDetails = [
     [50000.00, 100.00, 200.00]
 ]
 
-Size = len(Account)
+Size = len(Account)-1
 
-########################################
 #validation of account ID
-def AccExists(name, password):
-    for i in range(len(Account)):
-        if Account[i][0]== name and Account[i][1]== password:
-            return i
-        '''else:
-            return -1'''
-        
-#will reapeat until the existence of the account has been confirmed
-name = input('Enter name: ')
-password = input('Enter password: ')
-valid = AccExists(name, password)
-while valid == None:
-    print('The name or password is incorrect. Please enter correct name and password')
-    name = input('Enter name: ')
-    password = input('Enter password: ')
-    valid = AccExists(name, password)
+def AccExists(AccID):
+    if AccID < 0 or AccID > Size:
+        return False
+    else:
+        return True
+
+def Login(name, password, AccID):
+    if Account[AccID][0] != name or Account[AccID][1] != password:
+        return False
+    else:
+        return True
 #'procedures' for all four actions
 def DisplayBalance(AccID):
-    print(f'\nThe balance is{ AccDetails[AccID][0]}\n')
+    print(f'\nThe balance is { AccDetails[AccID][0]}\n')
 def WithDrawMoney(AccID):
     if AccDetails[AccID][0] >= -(AccDetails[AccID][1]):
         withdraw_amount = float(input('\nHow much would you like to withdraw? '))
@@ -45,22 +36,34 @@ def WithDrawMoney(AccID):
         AccDetails[AccID][0] -= withdraw_amount
         print(f'\n{withdraw_amount} has been successfully withdrawn from your account\n')
     else:
-        print('\nYou have reached the overdraft limit. To deposit money, press 3.')
+        print('\nYou have reached the overdraft limit. To deposit money, press 3.\n')
 def DepositMoney(AccID):
     deposit_money = float(input('\nHow much would you like to deposit? '))
     AccDetails[AccID][0] += deposit_money
     print(f'\n{deposit_money} have been successfully deposited\n')
-#display menu
-option = 0
-while option != 4:
-    option = int(input('''Enter 1 to display balance
+#test
+cusID = int(input('Enter account ID: '))
+if AccExists(cusID):
+    name  = input('Enter name: ')
+    password = input('Enter password: ')
+    if Login(name, password, cusID):
+        #display menu
+        option = 0
+        while option != 4:
+            option = int(input('''Enter 1 to display balance
 Enter 2 to withdraw money
 Enter 3 to deposit money
 Enter 4 to exit
 '''))
-    if option == 1:
-        DisplayBalance(valid)
-    elif option == 2:
-        WithDrawMoney(valid)
-    elif option == 3:
-        DepositMoney(valid)
+            if option == 1:
+                DisplayBalance(cusID)
+            elif option == 2:
+                WithDrawMoney(cusID)
+            elif option == 3:
+                DepositMoney(cusID)
+            else:
+                print('You can only enter 1, 2, 3, or 4.')
+    else:
+        print('Incorrect name or password.')
+else:
+    print('Incorrect accout ID/Account with this ID does not exist')
